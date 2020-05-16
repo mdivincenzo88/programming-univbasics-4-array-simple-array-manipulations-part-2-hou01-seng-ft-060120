ca20ddefcaf09fc7ba1479aa9dd46825ec7d0bd0
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-16 04:15:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat (first_array, second_array)
+  first_array.concat(second_array)
+end
+
+def using_insert(array,new_element)
+  array.insert(4,new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,new_string)
+  array.delete(new_string)
+end
+
+def using_delete_at(array,integer_index)
+  array.delete_at(integer_index)
+end
