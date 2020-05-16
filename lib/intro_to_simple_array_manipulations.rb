f1f0e53eecb60a5f3b2d1824ff9e65f7249efe27
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-16 03:59:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat (first_array, second_array)
   first_array.concat(second_array)
 end
 
-def using_insert(array,element)
+def using_insert(array,new_element)
   array.insert(3,element)
 end
